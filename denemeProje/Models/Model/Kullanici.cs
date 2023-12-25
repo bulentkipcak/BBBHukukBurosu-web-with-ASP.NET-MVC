d33a6ff7180385faaf888e61346e19f7bd1b5f15
bülent <95385514+bulentkipcak@users.noreply.github.com>
2023-12-25 10:45:46 +0300

    added login and registiration page for blog
    i used session, changed some colors in page

---
+++ denemeProje/Models/Model/Kullanici.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace denemeProje.Models.Model
+{
+    [Table("Kullanici")]
+    public class Kullanici
+    {
+
+        [Key]
+        public int KullaniciId { get; set; }
+        [Required]
+        public string KullaniciAd {  get; set; }
+        [Required]
+        public string Sifre { get; set; }
+
+    }
+}
