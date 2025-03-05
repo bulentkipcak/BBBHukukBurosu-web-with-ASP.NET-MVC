01e07735b229bcccbcf60dec453e0760cb67356e
bülent <95385514+bulentkipcak@users.noreply.github.com>
2025-03-05 23:03:49 +0300

    changed the project name

---
+++ BBBHukuk/Controllers/HakkimizdaController.cs
@@ -0,0 +1,43 @@
+﻿using BBBHukuk.Models.DataContext;
+using BBBHukuk.Models.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BBBHukuk.Controllers
+{
+    public class HakkimizdaController : Controller
+    {
+        BBBHukukBurosuDBContext db = new BBBHukukBurosuDBContext();
+        // GET: Hakkimizda
+        public ActionResult Index()
+        {
+            var h = db.Hakkimizda.ToList();
+            return View(h);
+
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var h = db.Hakkimizda.Where(x => x.HakkimizdaId == id).FirstOrDefault();
+            return View(h);  
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ValidateInput(false)]
+        public ActionResult Edit(int id, Hakkimizda h)
+        {
+            if (ModelState.IsValid)
+            {
+                var hakkimizda = db.Hakkimizda.Where(x => x.HakkimizdaId == id).SingleOrDefault();
+                hakkimizda.Aciklama = h.Aciklama;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            
+            return View(h);
+        }
+    }
+}
---
+++ BBBHukuk/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BBBHukuk.MvcApplication" Language="C#" %>
